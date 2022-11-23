fa36d262b9ea3e765fae5911edc81e058e00501d
Denis4pf <denis.onwonga@student.moringaschool.com>
2022-11-23 16:38:17 +0300

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: "Dwight", last_name: "Schrute", grade: 2)
+Student.create!(first_name: "Chris", last_name: "Ngoma", grade: 3)
